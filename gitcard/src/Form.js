a4a1eae22bf1e1b1d00e59aa957e37cf0a1c7329
Manish Kumar Tiwari <matul31517@gmail.com>
2023-03-21 23:23:59 +0530

    added submit button

--- gitcard/src/Form.js
+++ gitcard/src/Form.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 class Form extends React.Component{
+    userNameInput = React.createRef()
+    handleSubmit = (event)=> {
+        event.preventDefault();
+        console.log(this.userNameInput.current.value);
+    };
     render(){
         return (
-            <form action="">
-              <input type="text" placeholder="Github Username" />
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="Github Username" 
+              ref={this.userNameInput}
+              />
               <button>Add Card</button>
 
             </form>
