c817a661da933a60987dd3e23e7d56197f14b3bf
Manish Kumar Tiwari <matul31517@gmail.com>
2023-03-22 23:42:12 +0530

    implementing the form compoennt

--- gitcard/src/Form.js
+++ gitcard/src/Form.js
@@ -1,22 +1,42 @@
 import React from 'react';
 
 class Form extends React.Component{
-    userNameInput = React.createRef()
-    handleSubmit = (event)=> {
+//  / userNameInput = React.createRef()
+//     handleSubmit = (event)=> {
+//         event.preventDefault();
+//         console.log(this.userNameInput.current.value);
+//     };
+//     render(){
+//         return (
+//             <form onSubmit={this.handleSubmit}>
+//               <input type="text" placeholder="Github Username" 
+//               ref={this.userNameInput}
+//               />
+//               <button>Add Card</button>
+
+//             </form>
+//         )
+//     }
+
+// Following code is similar to above commented code
+   state = {userName : ''}
+   handleSubmit = (event)=> {
         event.preventDefault();
-        console.log(this.userNameInput.current.value);
+        console.log(this.state.userName);
     };
     render(){
         return (
             <form onSubmit={this.handleSubmit}>
               <input type="text" placeholder="Github Username" 
-              ref={this.userNameInput}
+              value={this.state.userName}
+              onChange = { event => this.setState({userName : event.target.value})}
               />
+    
               <button>Add Card</button>
 
             </form>
-        );
-    } 
+        )
+    }
 }
 
 export default Form;
